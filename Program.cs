3f7a365aafc0ec8c3889784132726edb4fe216db
Mateusz Ciechan <Mateusz.Ciechan@cdn.com.pl>
2021-04-20 22:24:07 +0200

    Final Version

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Task for Mateusz Ciechan
+ * Author: Mateusz Ciechan
+ * Completion Date: 20.04.2021
+ */
+
+namespace TaskMateuszCiechan
+{
+    public class AutomationTests
+    {
+        static void Main(string[] args)
+        {
+            LoadingPageTest loadingPageTest = new LoadingPageTest();
+            LoginTest loginTest = new LoginTest();
+            Orders orders = new Orders();
+
+            loadingPageTest.LoadPage("https://www.saucedemo.com/"); //load page test
+
+            loginTest.SuccesfulLogIn(); //succesful login test
+
+            loginTest.UnsuccesfulLogIn(); //unsuccesful login test
+
+            loginTest.LockedUserLogIn(); //locked user login test
+
+            loginTest.LogInWithoutTypingLogin(); //login without login test
+
+            loginTest.LogInWithoutTypingPassword(); //login without password test
+
+            orders.AddThreeItemsToCart(); //adding 3 items to cart test
+
+            orders.RemovingItemFromCart(); //removing item from cart test
+
+            orders.CheckoutAttempt(); //checkout attempt test
+
+            orders.CheckoutAttemptWithInvalidPostalCode(); //checkout with invalid postal code test
+
+            orders.MakeAnOrder(); //making an order test
+
+            orders.CheckIfCorrectItemsAreInCart(); //checking of correct items test
+
+            orders.CheckIfThereIsCorrectPrice(); //checking of correct price
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Abu uruchomić program, należy pobrać repozytorium, rozpakować w wybrane miejsce, wejść w folder \bin\Release\netcoreapp3.1, po czym uruchomić plik wykonywalny TaskMateuszCiechan.exe.
+Po wykonaniu programu, w głównym folderze TaskMateuszCiechan, powinien pojawić się bardzo podstawowy plik tekstowy z wynikami testów. 
+Do uruchomienia programu potrzebne jest środowisko uruchomieniowe .NET runtime w wersji 3.1, które można pobrać pod linkiem 
+https://dotnet.microsoft.com/download/dotnet/thank-you/runtime-3.1.14-windows-x64-installer
